e3b70b2244f87eac284fd53dd62d2c46be5a5298
SnovaDejaVu <snova.deja.vu@gmail.com>
2022-02-10 22:33:39 +0300

    password_generator

---
+++ password_generator.py
@@ -0,0 +1,22 @@
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the Password Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+
+random_letters = random.sample(letters,nr_letters)
+random_numbers = random.sample(numbers, nr_numbers)
+random_symbols = random.sample(symbols, nr_symbols)
+
+final_password = random_letters + random_numbers + random_symbols
+
+random.shuffle(final_password)
+final_password = "".join(final_password)
+
+print(f"Your password is \"{final_password}\"")
+
+
